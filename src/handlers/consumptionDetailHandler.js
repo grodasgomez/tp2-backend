834bf8aa3f558b99af9320981d063505bf9d3c43
diemanuel99 <diemanuel58@gmail.com>
2023-06-08 18:21:19 -0400

    postman and fix consumption detail handler, post and get

--- src/handlers/consumptionDetailHandler.js
+++ src/handlers/consumptionDetailHandler.js
@@ -8,17 +8,18 @@ import ValidationError from "../errors/ValidationError.js";
 import ApiError from "../errors/ApiError.js";
 
 export const getAll = () => {
-  return Consumption.findAll();
+  return ConsumptionDetail.findAll();
 };
 
 export const getByConsumptionId = (req) => {
-  return Consumption.findOne({
+  return ConsumptionDetail.findAll({
     where: { consumptionId: req.params.id },
   });
 };
 
 export const create = async (req) => {
   const validator = new ValidatorJs(req.body, {
+    consumptionId: "required|integer",
     productId: "required|integer",
     quantity: "required|integer",
   });
@@ -27,7 +28,7 @@ export const create = async (req) => {
   }
 
   const payload = req.body;
-  const foundProduct = Product.findByPk(payload.productId);
+  const foundProduct = await Product.findByPk(payload.productId);
 
   if (!foundProduct) {
     throw new ApiError("Product not found", 404);
@@ -37,9 +38,8 @@ export const create = async (req) => {
     throw new ApiError("Quantity must be greater than 0", 400);
   }
 
-  const consumptionId = req.params.id;
+  const consumptionId = payload.consumptionId;
   const foundConsumption = await Consumption.findByPk(consumptionId);
-
   foundConsumption.increment("total", {
     by: payload.quantity * foundProduct.price,
   });
